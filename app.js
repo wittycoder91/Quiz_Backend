5dd801a38f3a4eacd4a7016d00ed22cd152cf07e
eliasdiek <diektech@gmail.com>
2025-07-11 05:15:58 +0900

    Update the app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
 app.use(express.static("public"));
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api", routes);
 
 connectToDatabase();
